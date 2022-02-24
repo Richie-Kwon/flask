72f7a678adabb5d03ea1f5e8e768ad8af5328fdc
richie <richie.kwon1@gmail.com>
2022-02-24 20:43:21 +0000

    tweet endpoint

--- flaskServer/singUp.py
+++ flaskServer/singUp.py
@@ -3,6 +3,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 app.users = {}
 app.id_count = 1
+app.tweets = []
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
 
 @app.route("/sign-up", methods = ['POST'])
 def sign_up():
@@ -13,3 +18,21 @@ def sign_up():
 
     return jsonify(new_user)
 
+@app.route('/tweet', methods = ['POST'])
+def tweet():
+    payload=request.json
+    user_id = int (payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return 'User not found', 400
+
+    if len(tweet) > 300:
+        return 'The length of tweets is over 300', 400
+    user_id = int(payload['id'])
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+
+    return '', 200
