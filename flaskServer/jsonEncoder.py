17e419b18b13ac091495b93da8cc62d03a0027a3
richie <richie.kwon1@gmail.com>
2022-02-28 20:56:21 +0000

    mysql

--- flaskServer/jsonEncoder.py
+++ flaskServer/jsonEncoder.py
@@ -2,6 +2,7 @@ from flask.json import JSONEncoder
 
 class CustomJSONEncoder(JSONEncoder):
     # Overwrite "default" methods
+    # Change the set to list because the set type can't be coverted into JSON.
     def default(self, obj):
         if isinstance(obj, set):
             return list(obj)
